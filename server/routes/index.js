2e08901b1503994f696e241e12e6d22b6cea055d
Daniel Abrão <dcerag@br.ibm.com>
2016-09-23 18:31:00 -0300

    configs

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,26 +4,29 @@
 
     module.exports = function (app, passport, mqtt, iot_configs) {
 
-        var client = mqtt.connect({
-            "clientId": "a-rasp-node",
-            "username": "dcerag",
-            "password": "123",
-            "clean": false,
-            "keepAlive": 600000
-        });
 
-        client.on("error", function (error) {
-            console.log(error);
-        });
 
-        client.subscribe("mqtt/demo");
-
-        client.on("message", function (topic, payload) {
-            console.log([topic, payload].join(": "));
-            client.end();
-        });
 
         setInterval(function () {
+            var client = '';
+            client = mqtt.connect({
+                "clientId": "a-rasp-node",
+                "username": "dcerag",
+                "password": "123",
+                "clean": false,
+                "keepAlive": 600000
+            });
+
+            client.on("error", function (error) {
+                console.log(error);
+            });
+
+            client.subscribe("mqtt/demo");
+
+            client.on("message", function (topic, payload) {
+                console.log([topic, payload].join(": "));
+                client.end();
+            });
             client.publish("mqtt/demo", "hello world!");
         }, 1000);
 
