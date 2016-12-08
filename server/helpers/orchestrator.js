1902fc1a1f82dd01abb8d7e9b312d98874c2ba2b
Daniel Abrão <dcerag@br.ibm.com>
2016-12-08 12:15:05 -0200

    sensor status

--- server/helpers/orchestrator.js
+++ server/helpers/orchestrator.js
@@ -6,22 +6,32 @@
 
     module.exports = function (app, iot_cloud, iot_local, io, usonic) {
 
-        var sensor;
+        var sensor = 0,
+            ledStatus = 0;
 
         usonic.init(function (error) {
             if (error) {
                 console.log(error);
             } else {
                 sensor = usonic.createSensor(18, 17, 1000);
-                setInterval(function() {
-                    console.log('Distance: ' + sensor().toFixed(2) + ' cm');
-                }, 1000);
             }
         });
 
+        setInterval(function () {
+            iot_cloud.publish("iot-2/evt/status/fmt/json", JSON.stringify({
+                "sensor": [{
+                    "ultrasonic": {
+                        "distance": sensor
+                    }
+                }],
+                "controller": {
+                    "led01": ledStatus
+                }
+            }));
+        }, 1000);
+
+
         iot_cloud.subscribe("iot-2/cmd/+/fmt/json");
-
-
 
         iot_cloud.on("message", function (topic, msg) {
             console.log("message received");
