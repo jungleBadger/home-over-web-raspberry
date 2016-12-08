f6dafc17e0d5b64db0986ab7dfdccfa4ad95932d
Daniel Abrão <dcerag@br.ibm.com>
2016-12-08 11:38:18 -0200

    presence

--- server.js
+++ server.js
@@ -37,7 +37,7 @@
 
 
     var Gpio = require('onoff').Gpio,
-        pir = new Gpio(17, 'in', 'both');
+        pir = new Gpio(27, 'in', 'both');
     pir.watch(function(err, value) {
         if (err) exit();
         console.log(value);
